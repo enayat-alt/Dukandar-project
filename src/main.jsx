1341904d391d6d72350bacf7a66260f36b42cd66
salimshaikh54458-jpg <salimshaikh54458@gmail.com>
2026-05-30 15:51:24 +0530

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 
-import { CartProvider } from "./context/CartContext.jsx";
+//import { CartProvider } from "./context/CartContext.jsx";
+import { CartProvider } from "./context/cartContext.jsx";
 
 import "./index.css";
 
