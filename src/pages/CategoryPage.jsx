b266f761eb53afacf7055dca97cd6fc30551a42f
salimshaikh54458-jpg <salimshaikh54458@gmail.com>
2026-05-30 14:01:57 +0530

    build

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import ProductCard from "../components/ProductCard";
-import { useGetProductsByCategoryQuery } from "../services/productApi";
+//import { useGetProductsByCategoryQuery } from "../services/productApi";
 
 const CategoryPage = () => {
   const { categoryName } = useParams();
